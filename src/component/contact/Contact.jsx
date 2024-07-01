de3c3df678ceac743bac7b7f67cf75b50730c047
Jackie <actions@github.com>
2024-07-01 14:12:28 -0400

    Demo Videos

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -1,8 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Contact.css';
 import video from '../../asset/branding/4779866-hd_1920_1080_30fps.mp4';
-import arrow from '../../asset/ui/Up Left.png'
-
+import arrow from '../../asset/ui/Up Left.png';
 
 const Contact = () => {
   const [formData, setFormData] = useState({
@@ -10,6 +9,8 @@ const Contact = () => {
     email: '',
     message: ''
   });
+
+  const [showPopup, setShowPopup] = useState(false);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -19,16 +20,32 @@ const Contact = () => {
     }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Implement form submission logic here (e.g., sending data to server)
-    console.log(formData); // Placeholder for demonstration, replace with actual submission code
-    // Reset form after submission
-    setFormData({
-      name: '',
-      email: '',
-      message: ''
-    });
+
+    try {
+      const response = await fetch('https://v1.nocodeapi.com/jackies3668/google_sheets/tDDIfTJPaRQIhmfe?tabId=Sheet1', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: "follow",
+        body: JSON.stringify([[formData.name, formData.email, formData.message, new Date().toLocaleDateString()]])
+      });
+      await response.json();
+      setFormData({
+        name: '',
+        email: '',
+        message: ''
+      });
+      setShowPopup(true);
+      setTimeout(() => {
+        setShowPopup(false);
+      }, 2000);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -42,7 +59,6 @@ const Contact = () => {
         </div>
         <div className="form-right">
           <form onSubmit={handleSubmit}>
-   
             <input
               type="text"
               id="name"
@@ -51,9 +67,8 @@ const Contact = () => {
               onChange={handleInputChange}
               placeholder='Name'
               required
+              autocomplete="off"
             />
-
-
             <input
               type="email"
               id="email"
@@ -62,9 +77,8 @@ const Contact = () => {
               onChange={handleInputChange}
               placeholder='Email'
               required
+              autocomplete="off"
             />
-
-        
             <textarea
               id="message"
               name="message"
@@ -73,14 +87,17 @@ const Contact = () => {
               placeholder='Message'
               rows="3"
               required
+              autocomplete="off"
             ></textarea>
-
             <button type="submit">Send <img src={arrow} alt="" /></button>
+            <div className={`popup ${showPopup ? 'unhide' : ''}`}>
+              Your message has been sent!
+            </div>
           </form>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Contact;
--- src/component/projects/Projects.jsx
+++ src/component/projects/Projects.jsx
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './Projects.css';
 import arrow from '../../asset/ui/Up Left.png';
-import video1 from '../../asset/projects/videos/test.mp4';
-import video2 from '../../asset/projects/videos/test.mp4';
-import video3 from '../../asset/projects/videos/test.mp4';
-import video4 from '../../asset/projects/videos/test.mp4';
-import video5 from '../../asset/projects/videos/test.mp4';
+import video1 from '../../asset/projects/videos/Glimmering Grove.mp4';
+import video2 from '../../asset/projects/videos/Fresh Mint.mp4';
+import video3 from '../../asset/projects/videos/Celestial Tarot.mp4';
+import video4 from '../../asset/projects/videos/Mentor A Promise.mp4';
+import video5 from '../../asset/projects/videos/Stardust.mp4';
 
 const Projects = () => {
   const [projectIndex, setProjectIndex] = useState(0);
   const [fadeClass, setFadeClass] = useState('fade-in');
   const videos = [video1, video2, video3, video4, video5];
+
+  const websites = [
+    'https://jackie3668.github.io/glimmering-grove/',
+    'https://jackie3668.github.io/fresh-mint/',
+    'https://jackie3668.github.io/celestial-tarot/',
+    'https://mentorapromise.org/',
+    'https://jackie3668.github.io/stardust/'
+  ];
 
   useEffect(() => {
     if (fadeClass === 'fade-out') {
@@ -29,12 +37,11 @@ const Projects = () => {
     const titles = document.querySelectorAll('.titles h1');
     titles.forEach(title => title.classList.remove('active'));
     e.target.classList.add('active');
-
     setFadeClass('fade-out');
   };
 
   const resetFadeIn = () => {
-    const videoElement = document.querySelector('.project-video');
+    const videoElement = document.querySelector('.display');
     videoElement.classList.remove('fade-in');
     void videoElement.offsetWidth; 
     videoElement.classList.add('fade-in');
@@ -54,8 +61,8 @@ const Projects = () => {
           <h1 onClick={handleClick} className='active' id='0' title='GLIMMERING GLOVE'>GLIMMERING GLOVE</h1>
           <h1 onClick={handleClick} id='1' title='FRESH MINT'>FRESH MINT</h1>
           <h1 onClick={handleClick} id='2' title='CELESTIAL TAROT'>CELESTIAL TAROT</h1>
-          <h1 onClick={handleClick} id='4' title='STARDUST'>STARDUST</h1>
           <h1 onClick={handleClick} id='3' title='MENTOR A PROMISE'>MENTOR A PROMISE</h1>
+          <h1 onClick={handleClick} id='4' title='STARDUST'>STARDUST</h1>        
         </div>
         <div className="display-wrapper">
           <div className="display">
@@ -67,14 +74,18 @@ const Projects = () => {
               loop
             ></video>
             <div className="button-wrapper">
-              <button className="website">
-                Website
-                <img src={arrow} alt="" />
-              </button>
-              <button className="case-study">
-                Case Study
-                <img src={arrow} alt="" />
-              </button>
+              <a href={websites[projectIndex]} target="_blank" rel="noopener noreferrer" className="website">
+                <button className="website">
+                  Website
+                  <img src={arrow} alt="" />
+                </button>
+              </a>
+              {/* <a href={websites[projectIndex]} target="_blank" rel="noopener noreferrer" className="website">
+                <button className="case-study">
+                  Case Study
+                  <img src={arrow} alt="" />
+                </button>
+              </a> */}
             </div>
           </div>
         </div>
